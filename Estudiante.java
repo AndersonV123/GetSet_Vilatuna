fff290110f2975af4297501632ec0fe86b8895a0
AndersonV123 <150813921+AndersonV123@users.noreply.github.com>
2023-12-01 22:05:58 -0500

    Constructor
    Constructor de la clase estudiante

--- Estudiante.java
+++ Estudiante.java
@@ -4,3 +4,9 @@ public class Estudiante {
     private String nombre;
     private int edad;
     private String curso;
+    public Estudiante(String nombre, int edad, String curso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.curso = curso;
+    }
+}
